2be16484b3eccd4319bc9b9420b4b1ed3421688a
Tahvohck <Tahvohck@gmail.com>
2020-06-09 15:42:57 -0400

    Initial commit, set up

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tahvohck_Mods.JPFariasUpdates
+{
+    public class SkipIntro
+    {
+
+    }
+}
